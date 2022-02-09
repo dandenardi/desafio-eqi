ffba9811e5540d2b07f531dab018d2e0e65c16af
Daniel do Amaral Denardi <dan.denardi@gmail.com>
2022-02-09 16:24:19 -0300

    insercao de dados via useEffect, geracao de resultados

--- src/App.js
+++ src/App.js
@@ -1,40 +1,84 @@
-import { useState, useEffect } from 'react';
-import axios from 'axios';
+import { useState, useEffect, useCallback } from 'react';
+import { Chart } from 'react-google-charts'
  
 import './App.css';
+import axios from 'axios';
 
+
+//fazer os estados dos resultados (5), baseados em rendimento e indexacao (restante eh fixo)
 
 function App() {
 
-  const [indicators, setIndicators] = useState({});
+  //estados da API
+  const [apiData, setApiData] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState('');
+  const [indicators, setIndicators] = useState([]);
   const [sim, setSim] = useState({});
-  const [result, setResult] = useState(false);
-  const [revenue, setRevenue] = useState('gross');
-  const [index, setIndex] = useState('post');
-  const [deadline, setDeadline] = useState(0);
-  const [fstContribution, setFirstContribution] = useState(0);
-  const [mthContribution, setMthContribution] = useState(0);
-  const [profit, setProfit] = useState(0);
+  const [simIndex, setSimIndex] = useState(0);
+  const [simOpt, setSimOpt] = useState('');
+  const [searchInput, setSearchInput] = useState('');
+  const [criteria, setCriteria] = useState('');
+  const [simBoxValues, setSimBoxValues] = useState({});
   
+
+  const [simChartValues, setSimChartValues] = useState({
+    withContribution: [{}],
+    withoutContribution: [{}]
+  })
+
+  const [result, setResult] = useState(false);
+  const [revenue, setRevenue] = useState('bruto');
+  const [index, setIndex] = useState('pre');
+  const [deadline, setDeadline] = useState(0);
+  const [fstContribution, setFstContribution] = useState(0);
+  const [mthContribution, setMthContribution] = useState(0);
+  const [selectData, setSelectData] = useState(0);
+
+
+  const fetchDataInd = useCallback (() => {
+    axios
+      .get('http://localhost:3000/indicadores')
+      .then(resp => {
+        setIndicators(resp.data)
+        setIsLoading(false)
+      }
+
+      )
+      .catch(error  => {
+        setError(error.message)
+        setIsLoading(false)
+      })
+  }, []) 
+
   useEffect(() => {
+    fetchDataInd()
+  }, [fetchDataInd])
     
-    const apiIndices = async () => {
-      const response = await axios.get('http://localhost:3000/indicadores')
-      setIndicators(response.data);
+  const fetchDataSim = useCallback (() => {
+    axios
+      .get('http://localhost:3000/simulacoes')
+      .then(resp => {
+        setSim(resp.data)
+        setIsLoading(false)
+      }
 
-    };
-    const apiSim = async () => {
-      const response = await axios.get('http://localhost:3000/simulacoes')
-      setSim(response.data);
+      )
+      .catch(error  => {
+        setError(error.message)
+        setIsLoading(false)
+      })
+  }, []) 
 
-    };
-    apiIndices();
-    apiSim();
-    //tentar chamar as funcoes apenas quando necessario
-  }, []);
+  useEffect(() => {
+    fetchDataSim()
+  }, [fetchDataSim])
 
-  const cdi = indicators[0];
-  const ipca = indicators[1];
+ 
+  if (error){
+    return <h1>Erro ao consultar a API: {error}</h1>
+  }
+
 
   function applyPercentage(num){
     let percentage;
@@ -44,8 +88,56 @@ function App() {
 
   }
 
-  function handleDeposit(){
-    console.log('handleDeposit called!');
+  function handleSimIndex(){
+    
+    if (index === 'pre' && revenue === 'bruto'){
+      setSimIndex(0);
+    }else if (index === 'pos' && revenue === 'bruto'){
+      setSimIndex(1);
+    }else if (index === 'ipca' && revenue === 'bruto'){
+      setSimIndex(2)
+    }else if (index === 'pre' && revenue === 'liquido'){
+      setSimIndex(3)
+    }else if (index === 'pos' && revenue === 'liquido'){
+      setSimIndex(4)
+    }else if (index === 'ipca' && revenue === 'liquido'){
+      setSimIndex(5)
+    }else{
+      console.log('Esta opção não retorna dados de simulação válidos!');
+    }
+
+  }
+
+  function handleSimData(){
+    
+    console.log('HandleSimData called!');
+  }
+
+
+  function handleFstCtb(e){
+    e.preventDefault();
+    console.log(e.target);
+    e.target.value = e.target.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');
+    
+    setFstContribution(e.target.value);
+    console.log(fstContribution);
+  }
+
+  function handleMthCtb(e){
+    e.preventDefault();
+    
+
+    e.target.value = e.target.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');
+    
+    setMthContribution(e.target.value);
+    console.log(mthContribution);
+  }
+
+  function handleDeadline(e){
+    e.preventDefault();
+    
+    setDeadline(e.target.value);
+    console.log(deadline);
   }
 
   function cleanFields(){
@@ -57,23 +149,102 @@ function App() {
   }
 
   function handleResults(){
-    console.log('handleResults called!')
-    console.log(revenue, index, deadline, fstContribution, mthContribution);
-  }
-
-  function showResults(){
-    console.log('Show results called!');
     
+    handleSimIndex();
+    console.log(simIndex);
+    handleSimData()
+
+    console.log('handleResults called!')
+    console.log(revenue, index, fstContribution, mthContribution);
+    setResult(true);
+  }
+  
+  function showResults(){
+    
+    let i = simIndex;
+    console.log('sim[i].graficoValores.comAporte')
+    const dataOld = [
+       ['joago', 'fernando'],
+       ['fiz', 1000],
+       ["Rachel", 100],
+      ["Patrick", 200],
+      ["Eric", 1900],
+    ];
+    
+    const dataNew = [
+      ["Name", "Popularity"],
+      ["Cesar", 370],
+      ["Rachel", 600],
+      ["Patrick", 700],
+      ["Eric", 1500],
+    ];
+    
+    let diffdata = {
+      old: dataOld,
+      new: dataNew,
+    };
+
     if(result === true){
       return(
-        <div>Resultados da consulta...</div>
+        <div className='results'>
+          <div>
+            <h2>Resultado da Simulação</h2>
+          </div>
+
+          <div className='data-boxes'>
+            <div className='box'>
+              <h3>Valor final Bruto</h3>
+              <p>{sim[i].valorFinalBruto}</p>
+            </div>
+                      
+            <div className='box'>
+              <h3>Aliquota do IR</h3>
+              <p>{sim[i].aliquotaIR}</p>
+            </div>
+            
+            <div className='box'>
+                <h3>Valor Pago em IR</h3>
+              <p>{sim[i].valorPagoIR}</p>
+            </div>
+            
+            <div className='box'>
+              <h3>Valor final Líquido</h3>
+              <p>{sim[i].valorFinalLiquido}</p>
+            </div>  
+          
+            <div className='box'>
+              <h3>Valor Total Investido</h3>
+              <p>{sim[i].valorTotalInvestido}</p>
+            </div>
+
+            <div className='box'>
+              <h3>Ganho Líquido</h3>
+              <p>{sim[i].ganhoLiquido}</p>
+            </div>
+          </div>
+
+          <div className='chart'>
+              <h3>Projeção de Valores</h3>
+               
+              <Chart 
+                chartType="ColumnChart"
+                width="100%"
+                height='400px'
+                diffdata={diffdata}
+                legendToggle
+              
+              />
+          </div>
+
+        </div>
+
       )
     }else{
       return(null);
     }
 
   }
-
+ 
   
   return (
     <div className="App">
@@ -83,26 +254,28 @@ function App() {
         <div className='yield'>
           <h6>Rendimento</h6>
           <div className='opt'>
-            <input type="radio" id="gross" name="revenue" value="gross" onClick={()=> setRevenue('gross')}/>
-            <label htmlFor="gross">Bruto</label>
-            <input type="radio" id="net" name="revenue" value="net" onClick={()=> setRevenue('net')}/>
-            <label htmlFor="net">Líquido</label>
+            <input type="radio" id="bruto" name="revenue" value="bruto" onClick={()=> setRevenue('bruto')}/>
+            <label htmlFor="bruto">Bruto</label>
+            <input type="radio" id="liquido" name="revenue" value="liquido" onClick={()=> setRevenue('liquido')}/>
+            <label htmlFor="liquido">Líquido</label>
         
           </div>
 
           <div className='input'>
-            <label>Aporte Inicial</label>
-            <input type='text'></input>
+            <label className='error-fst-ctb' id='fst-ctb-label'>Aporte Inicial</label>
+            <input type='text' onChange={handleFstCtb}></input>
+            <div className='error-fst-ctb'></div>
           </div>
           
           <div className='input'>
             <label>Prazo (em meses)</label>
-            <input type='text'></input>
+            <input type='text' pattern="[0-9]+" onChange={handleDeadline}></input>
           </div>
           
           <div className='input'>
-            <label>{ipca.nome}</label>
-            <label>{applyPercentage(ipca.valor)}</label>
+            <label>CDI (ao ano)</label>
+            <input type="radio" id="IPCA" name="index" value="10,06%" onClick={()=> setIndex(indicators[1].nome)}/>
+            <label htmlFor="cdi">10,06%</label>
             
           </div>
           
@@ -117,25 +290,26 @@ function App() {
           <div className='opt'>
             <input type="radio" id="pre" name="index" value="pre" onClick={()=> setIndex('pre')}/>
             <label htmlFor="pre">PRÉ</label>
-            <input type="radio" id="post" name="index" value="post" onClick={()=> setIndex('post')}/>
+            <input type="radio" id="post" name="index" value="post" onClick={()=> setIndex('pos')}/>
             <label htmlFor="post">PÓS</label>
-            <input type="radio" id="fixed" name="index" value="fixed" onClick={()=> setIndex('fixed')}/>
+            <input type="radio" id="fixed" name="index" value="fixed" onClick={()=> setIndex('fixado')}/>
             <label htmlFor="fixed">FIXADO</label>
           </div>
 
           <div className='input'>
-            <label>Aporte Mensal</label>
-            <input type='text' onChange={handleDeposit}></input>
+            <label id='mth-ctb'>Aporte Mensal</label>
+            <input type='text' pattern="[0-9]+" onChange={handleMthCtb}></input>
           </div>
           
           <div className='input'>
             <label>Rentabilidade</label>
-            <input type='text' onChange={handleProfit}></input>
+            <input type='text' pattern="[0-9]+" onChange={handleProfit}></input>
           </div>
           
           <div className='input'>
-            <label>{cdi.nome} (ao ano)</label>
-            <label>{applyPercentage(cdi.valor)}</label>
+            <label>CDI (ao ano)</label>
+            <input type="radio" id="CDI" name="index" value="9,15%" onClick={()=> setIndex(indicators[0].nome)}/>
+            <label htmlFor="cdi">9,15%</label>
             
           </div>
           
