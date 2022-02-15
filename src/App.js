98406730353c9b6fe368d89c3be7a0c55c04dc60
Daniel do Amaral Denardi <dan.denardi@gmail.com>
2022-02-15 17:44:16 -0300

    Implementada a geracao de grafico e validacoes em formulario

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { useState, useEffect } from 'react';
+import { Chart as ChartJS, CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend, } from 'chart.js';
+import { Bar } from 'react-chartjs-2';
 
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import ButtonGroup from '@mui/material/ButtonGroup';
-
+import { InputAdornment } from '@mui/material';
 
 import './App.css';
 import axios from 'axios';
+import { findAllByTestId } from '@testing-library/react';
+
 
 
 const api = axios.create({
@@ -14,18 +18,23 @@ const api = axios.create({
 });
 
 
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
+
+
 function App() {
 
   //estados da API
-  const [apiData, setApiData] = useState({});
-  const [isLoading, setIsLoading] = useState(true);
-  const [apiWait, setApiWait] = useState(false);
+ 
   const [indicators, setIndicators] = useState(null);
   const [sim, setSim] = useState(null);
   const [simIndex, setSimIndex] = useState(0);
-  const [searchInput, setSearchInput] = useState('');
-  const [criteria, setCriteria] = useState('');
-  const [simBoxValues, setSimBoxValues] = useState({});
   
   const [comAporteObj, setComAporteObj] = useState([]);
   const [semAporteObj, setSemAporteObj] = useState([]);
@@ -35,11 +44,14 @@ function App() {
   const [result, setResult] = useState(false);
   const [revenue, setRevenue] = useState('bruto');
   const [index, setIndex] = useState('pre');
-  const [deadline, setDeadline] = useState(0);
-  const [profitability, setProfitability] = useState(0)
-  const [fstContribution, setFstContribution] = useState(0);
-  const [mthContribution, setMthContribution] = useState(0);
-  const [selectData, setSelectData] = useState(0);
+  const [deadline, setDeadline] = useState('');
+  const [deadlineError, setDeadlineError] = useState(false)
+  const [profitability, setProfitability] = useState('')
+  const [profitabilityError, setProfitabilityError] = useState(false)
+  const [fstContribution, setFstContribution] = useState('');
+  const [fstContributionError, setFstContributionError] = useState(false)
+  const [mthContribution, setMthContribution] = useState('');
+  const [mthContributionError, setmthContributionError] = useState(false)
 
 
   useEffect(() => {
@@ -77,13 +89,12 @@ function App() {
     }
     getIndicators();
   }, []);
-  
 
-  function applyPercentage(num){
-    let percentage;
-    percentage = num.toLocaleString('pt-br');
-    percentage = percentage + '%'; 
-    return percentage;
+  function applyRealConfig(num){
+    let valueInReal;
+    valueInReal = num.toLocaleString('pt-br', { minimumFractionDigits: 2 , style: 'currency', currency: 'BRL' });
+    
+    return valueInReal;
 
   }
 
@@ -105,7 +116,7 @@ function App() {
       simIndex = 5;
     }else{
       console.log('Esta opção não retorna dados de simulação válidos!');
-      simIndex = null;
+      simIndex = 0;
     }
     
     return simIndex;
@@ -113,35 +124,60 @@ function App() {
 
   }
 
+  function handleSubmit(e){
+
+    e.preventDefault();
+    console.log('HandleSubmit chamado!');
+    if(isNaN(fstContribution)){
+      setFstContributionError(true);
+      return false; 
+    } 
+    
+    if(isNaN(mthContribution)){
+      setmthContributionError(true);
+      return false;
+    } 
+
+    if(isNaN(profitability)){
+      setProfitabilityError(true);
+      return false;
+    }
+    
+    if(isNaN(deadline)){
+      setDeadlineError(true);
+      return false;
+    }
+
+    handleResults();
+  }
 
   function handleFstCtb(e){
     e.preventDefault();
-    console.log(e.target);
-    e.target.value = e.target.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');
-    
+    setFstContributionError(false);
     setFstContribution(e.target.value);
-    console.log(fstContribution);
+    
   }
 
   function handleMthCtb(e){
     e.preventDefault();
-    
-
-    e.target.value = e.target.value.replace(/[^0-9.]/g, '').replace(/(\..*)\./g, '$1');
+    setmthContributionError(false)
     
     setMthContribution(e.target.value);
-    console.log(mthContribution);
   }
 
-  function handleDeadline(e){
-    e.preventDefault();
-    
-    setDeadline(e.target.value);
-    console.log(deadline);
-  }
 
   function cleanFields(){
+   
     console.log('cleanFields called!');
+    setFstContribution('');
+    setFstContributionError(false);
+    setMthContribution('');
+    setmthContributionError(false);
+    setDeadline('');
+    setDeadlineError(false);
+    setProfitability('');
+    setProfitabilityError(false);
+    
   }
 
   function handleProfit(){
@@ -152,34 +188,17 @@ function App() {
     
     let i = simIndex;
     
-    let comAporteStr = Object.keys(comAporteObj[i]).map(function (key) {
-          
-      // Using Number() to convert key to number type
-      // Using obj[key] to retrieve key value
-      return [Number(key), comAporteObj[i][key]];
-
-    });
-
-    let semAporteStr = Object.keys(comAporteObj[i]).map(function (key) {
-          
-      // Using Number() to convert key to number type
-      // Using obj[key] to retrieve key value
-      return [Number(key), comAporteObj[i][key]];
-
-    });
-    comAporteStr.unshift(['mes','valor']);
-    semAporteStr.unshift(['mes','valor']);
-    console.log(semAporteStr);
-    setComAporte(comAporte => (comAporteStr));
-    setSemAporte(semAporte => (semAporteStr));
+    setComAporte(comAporte => (comAporteObj[i]));
+    setSemAporte(semAporte => (semAporteObj[i]));
 
     return(comAporte, semAporte);
   }
 
+
   function handleResults(){
 
     let i = handleSimIndex();
-    console.log('handleResults called!')
+    console.log(i);
     setSimIndex(i);
     handleChartData();
     setResult(true);
@@ -188,10 +207,66 @@ function App() {
 
   function showResults(){
     
+       
+    const options = {
+      
+      plugins: {
+        
+        title: {
+          display: true,
+          text: 'Projeção de Valores',
+          position: 'top',
+        },
+        legend: {
+          position: 'bottom', 
+        },
+      },
+      responsive: true,
+      interaction: {
+        mode: 'index',
+        intersect: false,
+      },
+      scales: {
+        x: {
+          stacked: true,
+          title: {
+            display: true,
+            text: 'Tempo (meses)',
+          },
+        },
+        y: {
+          stacked: true,
+          title: {
+            display: true,
+            text: 'Valor (R$)',
+          },
+        },
+      },
+    };
+    
+    const labels = [];
+    
+    const data = {
+      labels,
+      datasets: [
+        {
+          label: 'Com Aporte',
+          data: comAporte,
+          backgroundColor: 'rgb(255, 102, 0)',
+          stack: 'Stack 0',
+        },
+        {
+          label: 'Sem Aporte',
+          data: semAporte,
+          backgroundColor: 'rgb(0, 0, 0)',
+          stack: 'Stack 0',
+        },
+        
+      ],
+    };
+    
 
     let i = simIndex; 
- 
-    
 
     if(result === true){
       return(
@@ -246,7 +321,7 @@ function App() {
             </div>
 
           <div className='chart'>
-            
+            <Bar options={options} data={data} />
           </div>
 
         </div>
@@ -267,8 +342,9 @@ function App() {
       <div className='simulator'>
         
         <div className='containers-holder'>
-            <h3>Simulador</h3>
-          <div className='containers'>
+          <h3>Simulador</h3>
+        
+          <form className='form' onSubmit={(e) => handleSubmit(e)}>
             
             <div className='container1'>
                   
@@ -283,24 +359,36 @@ function App() {
     
               <div className='input'>
                 <TextField 
+                  required
                   id="standard-basic" 
                   label="Aporte inicial" 
                   variant="standard" 
-                  onChange={handleFstCtb}/>
+                  value={fstContribution}
+                  onChange={handleFstCtb}
+                  error={fstContributionError}
+                  helperText={fstContributionError ? "Precisa ser número!" : ""}
+                  
+                />
+                
               </div>
                 
               <div className='input'>
                 <TextField 
+                  required
                   id="standard-basic" 
                   label="Prazo (em meses)" 
                   variant="standard" 
+                  value={deadline}
                   onChange={(e)=>{setDeadline(e.target.value)}}
+                  error={deadlineError}
+                  helperText={deadlineError ? "Precisa ser número!" : ""}
                 />
               </div>
                 
               <div className='input'>
                 
                 <TextField
+                  required
                   id="outlined-read-only-input"
                   label="IPCA (ao ano)"
                   defaultValue="10,06%"
@@ -320,7 +408,7 @@ function App() {
             
               <h6 className='h6-title'>Tipos de indexação</h6>
               <div className='opt'>
-                 <ButtonGroup variant="contained" aria-label="tipos de indexadores">
+                <ButtonGroup variant="contained" aria-label="tipos de indexadores" value={index}>
                     <Button  onClick={()=> setIndex('pre')}>PRÉ</Button>
                     <Button  onClick={()=> setIndex('pos')}>PÓS</Button>
                     <Button  onClick={()=> setIndex('fixado')}>FIXADO</Button>
@@ -329,20 +417,28 @@ function App() {
 
               <div className='input'>
                 <TextField 
+                  required
                   id="standard-basic" 
                   label="Aporte Mensal" 
                   variant="standard" 
+                  value={mthContribution}
                   onChange={handleMthCtb}
+                  error={mthContributionError}
+                  helperText={mthContributionError ? "Precisa ser número!" : ""}
                 />
 
               </div>
               
               <div className='input'>
                 <TextField 
+                  required
                   id="standard-basic" 
                   label="Rentabilidade" 
                   variant="standard" 
+                  value={profitability}
                   onChange={(e)=>{setProfitability(e.target.value)}}
+                  error={profitabilityError}
+                  helperText={profitabilityError ? "Precisa ser número!" : ""}
                 />
               </div>
               
@@ -360,12 +456,14 @@ function App() {
               </div>
               
               <div className='btn'>
-                <Button variant="contained" onClick={handleResults}>Simular</Button>
+                <Button variant="contained" type='submit'>Simular</Button>
               </div>
 
             </div>
             
-          </div>
+          </form>
+        
+          
         </div>
         
         <div className='results'>
